9f48c8e541f0e62d1bea683c64471a2cd4d6175a
erinaldo.souza <erinaldo.souza@gmail.com>
2016-02-20 09:48:24 -0200

    Tela de login Tela de cadastro e usuários Internacionalizaçõ implementada

---
+++ src/br/com/cursowebfa7/filtro/Autenticador.java
@@ -0,0 +1,44 @@
+package br.com.cursowebfa7.filtro;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/*")
+public class Autenticador  implements Filter {
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		//TODO implementar logica de autenticação.
+		
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpSession session = httpRequest.getSession();
+		
+		String requesrUrl = httpRequest.toString();
+		chain.doFilter(request, response);
+	}
+	
+
+	@Override
+	
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/br/com/cursowebfa7/listener/AppListener.java
@@ -0,0 +1,62 @@
+package br.com.cursowebfa7.listener;
+
+import java.util.HashMap;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import br.com.cursowebfa7.model.Usuario;
+
+/**
+ * Application Lifecycle Listener implementation class AppListener
+ *
+ */
+@WebListener
+public class AppListener implements ServletContextListener {
+	
+	/**
+     * Default constructor. 
+     */
+    public AppListener() {
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+     */
+    public void attributeAdded(ServletContextAttributeEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+     */
+    public void attributeRemoved(ServletContextAttributeEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+     */
+    public void attributeReplaced(ServletContextAttributeEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+ 
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent servlet)  { 
+    	servlet.getServletContext()
+    			.setAttribute("usuariosMap", new HashMap<String, Usuario>());
+    }
+	
+}
